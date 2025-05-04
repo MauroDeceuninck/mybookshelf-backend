2129bca5dad0471582a0af553ec9a2cf140aac18
MauroDeceuninck <mauro.deceuninck@student.vives.be>
2025-05-04 18:18:50 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("MongoDB connection error:", err));
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+
+module.exports = app;
